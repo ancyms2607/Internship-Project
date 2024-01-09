8c4f13df8ba52a8af15b9baa8bc148a29e8d23d9
Fathima-Neserin <145750255+Fathima-Neserin@users.noreply.github.com>
2024-01-09 09:37:27 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const  mongoose=require('mongoose');
-mongoose.connect('mongodb+srv://ictak123:ictak12@cluster0.fthdolr.mongodb.net/Internshipportal?retryWrites=true&w=majority')
-.then(()=>{
-    console.log("DB connected successfully");
-})
-.catch((error)=>{
-    console.log("Error! Not connected!!",error);
-})
