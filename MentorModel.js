88e28c10d95d73f89e406527dbb709070a4da65c
Fathima-Neserin <145750255+Fathima-Neserin@users.noreply.github.com>
2024-01-09 09:36:18 +0530

    Delete MentorModel.js

--- MentorModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const  MentorSchema=mongoose.Schema({
-    email:String,
-    password:String
-})
-
-const MentorData=mongoose.model('mentordata',MentorSchema);
-module.exports=MentorData;
