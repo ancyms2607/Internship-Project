b245d6a30b80feebb00139f40dac1586ec85fc30
Fathima Neserin <fathimanezrin2001@gmail.com>
2024-01-19 23:29:36 +0530

    Update GradeRoutes.js
    del

--- routes/api/GradeRoutes.js
+++ routes/api/GradeRoutes.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const GradeController = require('../../controllers/GradeControllers');
-const verifyJWT=require('../../middleware/verifyJWT')
 
-router.route('/')
-    .get(verifyJWT,GradeController.getAllGrades)
 
 
 
